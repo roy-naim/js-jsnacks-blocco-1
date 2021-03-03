476b6efa90532bb51624dff36e7ddf568e25075d
Roy <b.roynaim@gmail.com>
2021-03-03 16:05:27 +0100

    Jsnacks-1 esercizio completato

---
+++ jsnacks-1/main.js
@@ -0,0 +1,9 @@
+var first_number = prompt("Dammi un numero");
+
+var second_number = prompt("Another one");
+
+if (first_number > second_number) {
+    console.log(first_number);
+  } else {
+    console.log(second_number);
+  }
