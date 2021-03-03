5c2bdf64b8e4b9b514dec20d3399e204b8f22041
Roy <b.roynaim@gmail.com>
2021-03-03 17:05:14 +0100

    jsnacks-6 esercizio completato

---
+++ jsnacks-6/main.js
@@ -0,0 +1,8 @@
+// Stampa il cubo dei primi N numeri, dove N è un numero
+// indicato dall’utente.
+
+var cube = parseInt(prompt("Dammi un numero"));
+
+var numbers = Math.pow(cube, 3);
+
+console.log(numbers);
